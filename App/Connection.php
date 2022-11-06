4f24cceb5fecd7a6d5498e51724fe74fd76f73ff
Rafael Duarte <rafa.duartef@outlook.com>
2022-11-06 16:29:54 -0300

    Ajustada conexão

--- App/Connection.php
+++ App/Connection.php
@@ -2,15 +2,13 @@
 
     namespace App;
 
-    use Exception;
-
     class Connection {
         public static function getDb() {
             try {
-                $conn = new \PDO("mysql:host=localhost;dbname=groweb;charset=utf8", "root", "");
+                $conn = new \PDO("mysql:host=127.0.0.1:3306;dbname=groweb;charset=utf8", "root", "");
                 return $conn;
             } catch(\PDOException $e) {
-
+                return false;
             }
         }
     }
--- vendor/MF/Model/Conteiner.php
+++ vendor/MF/Model/Conteiner.php
@@ -6,7 +6,12 @@
             // retornar o modelo selecionado já instanciado e com a conexão estabelecida
             $class = "\\App\\Models\\" . ucfirst($model);
             $conn = Connection::getDb();
-            return new $class($conn);
+            if($conn != false) {
+                return new $class($conn);
+            }
+            else {
+                return false;
+            }
         }
     }
 
